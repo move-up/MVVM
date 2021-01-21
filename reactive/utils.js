996adcd4ad759d98296a85e9323c204cbfa13a93
liwanzhen <2274036472@qq.com>
2021-01-21 15:55:09 +0800

    删除

--- reactive/utils.js
+++
@@ -1,8 +0,0 @@
-function isObj(obj) {
-  return obj !== null && typeof obj === 'object'
-}
-
-const rawToReactive = new WeakMap()
-const reactiveToRaw = new WeakMap()
-const targetMap = new WeakMap()
-const effectStack = []
